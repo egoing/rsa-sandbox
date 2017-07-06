fa2bbfcd81c363d45c97598dccbf9c8f18bced93
Ubuntu <ubuntu@ip-172-31-14-21.ap-northeast-2.compute.internal>
2017-07-06 14:59:32 +0000

    pdf download

--- pdf.php
+++ pdf.php
@@ -6,15 +6,29 @@ require 'vendor/autoload.php';
 // reference the Dompdf namespace
 use Dompdf\Dompdf;
 
-// instantiate and use the dompdf class
-$dompdf = new Dompdf();
-$dompdf->loadHtml('hello world');
+// read html 
+ob_start();
+//be sure this file exists, and works outside of web context etc.)
+require("/var/www/html/pdf.html");
+$html = ob_get_clean();
 
-// (Optional) Setup the paper size and orientation
-$dompdf->setPaper('A4', 'landscape');
+if (isset($_GET['name'])) {
+	$html = str_replace('{{ $name }}', $_GET['name'], $html);
+}
 
-// Render the HTML as PDF
-$dompdf->render();
+if (isset($_GET['type']) && $_GET['type'] == 'pdf') {
+	// instantiate and use the dompdf class
+	$dompdf = new Dompdf();
+	$dompdf->load_html($html);
 
-// Output the generated PDF to Browser
-$dompdf->stream();
+	// (Optional) Setup the paper size and orientation
+	$dompdf->setPaper('A4', 'landscape');
+
+	// Render the HTML as PDF
+	$dompdf->render();
+
+	// Output the generated PDF to Browser
+	$dompdf->stream('certificate.pdf');
+} 
+
+echo $html; 
